ce1ba923c2d01278d917b5577b833091ddcb9e8a
AdelmoNascimento <50125688+AdelmoNascimento@users.noreply.github.com>
2020-08-05 15:52:03 -0300

    Desenvolvendo as Classes de peladas e usuários
    Dando inicio da criação entre as classes.

---
+++ Peladas.java
@@ -0,0 +1,51 @@
+package peladas;
+
+import java.util.Date;
+
+public class Peladas {
+
+	private String nomeEventEsportivo;
+
+	private Date dataEventEsportivo;
+
+	private int horaDoEventoEsportivo;
+
+	private String localDoEvento;
+
+	public Peladas() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNomeEventEsportivo() {
+		return nomeEventEsportivo;
+	}
+
+	public void setNomeEventEsportivo(String nomeEventEsportivo) {
+		this.nomeEventEsportivo = nomeEventEsportivo;
+	}
+
+	public Date getDataEventEsportivo() {
+		return dataEventEsportivo;
+	}
+
+	public void setDataEventEsportivo(Date dataEventEsportivo) {
+		this.dataEventEsportivo = dataEventEsportivo;
+	}
+
+	public int getHoraDoEventoEsportivo() {
+		return horaDoEventoEsportivo;
+	}
+
+	public void setHoraDoEventoEsportivo(int horaDoEventoEsportivo) {
+		this.horaDoEventoEsportivo = horaDoEventoEsportivo;
+	}
+
+	public String getLocalDoEvento() {
+		return localDoEvento;
+	}
+
+	public void setLocalDoEvento(String localDoEvento) {
+		this.localDoEvento = localDoEvento;
+	}
+
+}
---
+++ Usuarios.java
@@ -0,0 +1,50 @@
+package peladas;
+
+public class Usuarios {
+
+	// Nome copleto do usuário
+	private String nomeCompleto;
+
+	private String apelido;
+
+	private String email;
+
+	private String senha;
+
+	public Usuarios() {
+
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getApelido() {
+		return apelido;
+	}
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
